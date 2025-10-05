39f81c416e024e08c3d8cea8e69a334d37db3457
Harshavardhan1999-cmd <your_github_email@example.com>
2025-10-05 16:21:13 +0000

    Wk 6 Bible Assignment

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,29 @@
+// import "./App.css";
+// import RandomButton from "/home/chandu/Restapiproj/vite-project/components/RandomButton"
+
+// function App() {
+//   return (
+//     <>
+//       <h1>Bible Verse Generator</h1>
+//       <RandomButton/>
+//     </>
+//   );
+// };
+
+// export default App;
+
+import "./App.css";
+import RandomButton from "/home/chandu/Restapiproj/vite-project/components/RandomButton";
+import RetrievalButton from "/home/chandu/Restapiproj/vite-project/components/RetrievalButton"; // Import new component
+
+function App() {
+  return (
+    <>
+      <h1>Bible Verse Generator</h1>
+      <RandomButton />
+      <RetrievalButton /> {/* Add the new component */}
+    </>
+  );
+};
+
+export default App;
